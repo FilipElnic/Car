c5958979e03aed34d5f6b4cb2c81316f53220993
Filip Elznic <elznicfilip@gmail.com>
2024-11-05 10:25:21 +0100

    first sidebar

--- carschool/src/App.jsx
+++ carschool/src/App.jsx
@@ -3,13 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      {" "}
-      <div className="container">
-        <div className="corner top-left">Top Left</div>
-        <div className="corner top-right">Top Right</div>
-        <div className="corner bottom-left">Bottom Left</div>
-        <div className="corner bottom-right">Bottom Right</div>
-      </div>
+      <div className="flex"></div>
     </>
   );
 }
