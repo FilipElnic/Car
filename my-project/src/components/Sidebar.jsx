3893a367c47c205a43402ced912101f054795f77
Filip Elznic <elznicfilip@gmail.com>
2024-11-07 11:57:37 +0100

    new install

---
+++ my-project/src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+function Sidebar() {
+  return (
+    <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary shadow">
+      <i>a</i>
+      <i>b</i>
+      <i>c</i>
+      <i>d</i>
+    </div>
+  );
+}
+
+export default Sidebar;
