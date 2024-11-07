3b2d11ec0b3810833d4aa78213b110f442711df2
Filip Elznic <elznicfilip@gmail.com>
2024-11-07 12:23:36 +0100

    quick save

---
+++ car-project/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import LandingPage from "./components/LandingPage.jsx";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p>lsdhkaldkjf</p>
+        <LandingPage />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ car-project/src/components/LandingPage.jsx
@@ -0,0 +1,9 @@
+function LandingPage() {
+  return (
+    <>
+      <div className="h-100 p-3 bg-black">Width 100%</div>
+    </>
+  );
+}
+
+export default LandingPage;
