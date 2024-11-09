db129b2e357df76074d9856620baa7387f599473
Filip Elznic <elznicfilip@gmail.com>
2024-11-09 19:13:12 +0100

    quick update 2

---
+++ car-project/src/components/Main2.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../App.css";
+
+function Main2() {
+  return (
+    <>
+      <div className="container-fluid main2 full-height"></div>
+    </>
+  );
+}
+
+export default Main2;
